00b5d3db630040da721948b8d80a8a4f11b13453
赵 志建 <2462437754@qq.com>
2023-11-30 08:27:44 +0000

    Update 蛋蛋.js

--- js/蛋蛋.js
+++ js/蛋蛋.js
@@ -2,7 +2,7 @@
 var rule = {
     title:'蛋蛋剧',
     // host:'https://www.dandanju.cc',
-    host:'https://www.dandanju.vip',
+    host:'https://www.dandanju.cc',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"a:eq(0)&&href");print(src);HOST=src',
     // url:'/show/fyclass--------fypage---.html',
     url:'/show/fyclassfyfilter.html',
